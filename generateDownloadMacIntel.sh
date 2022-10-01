d6f18c764776398ca46fb0de29e0c00827a29563
Валерий Хегай <valerijhegaj@Air-Valerij.mshome.net>
2022-10-01 22:37:39 +0300

    compiling on mac intel

--- generateDownloadMacIntel.sh
+++ generateDownloadMacIntel.sh
@@ -1,8 +1,8 @@
 #on intel macbook
 
-fyne package
+go build -o checkers
 
 MAC_BIN_PATH="chekers.app/Contents/MacOS"
 
 mkdir $MAC_BIN_PATH/saves
-tar -czf download/macos-amd64/checkers.tar.gz $MAC_BIN_PATH/chekers $MAC_BIN_PATH/saves
+tar -czf download/macos-amd64/checkers.tar.gz checkers $MAC_BIN_PATH/saves
